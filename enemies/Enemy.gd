a957490474fa279e21526a9ae19ae14e4a1ee497
Dawn Wilson <16958583+dawnwilson@users.noreply.github.com>
2022-02-07 21:20:26 -0400

    Created enemy

---
+++ enemies/Enemy.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+var tile_size = 6
+
+func _ready():
+	position = position.snapped(Vector2.ONE * tile_size)
+	position += Vector2.ONE * tile_size/2
+
+
+func _on_Timer_timeout() -> void:
+	position += Vector2.LEFT * tile_size
