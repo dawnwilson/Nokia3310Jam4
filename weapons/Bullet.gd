7bbcf748c3a8b5d17abc0d99ac2ce6f5851ac838
Dawn Wilson <16958583+dawnwilson@users.noreply.github.com>
2022-02-12 15:31:56 -0400

    Bullet can destroy enemy

--- weapons/Bullet.gd
+++ weapons/Bullet.gd
@@ -10,3 +10,9 @@ func _on_MoveSpeed_timeout() -> void:
 
 func _on_VisibilityNotifier2D_screen_exited() -> void:
 	queue_free()
+
+
+func _on_Bullet_body_entered(body: Node) -> void:
+	if body.is_in_group("Enemies"):
+		body.queue_free()
+		queue_free()
