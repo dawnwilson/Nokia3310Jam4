0c6ceb36ade44453d9fb3adb7a98ca0d4d5384a1
Dawn Wilson <16958583+dawnwilson@users.noreply.github.com>
2022-02-12 15:46:05 -0400

    Basic laser beam working where the laser destroys the enemies

--- weapons/Laser.gd
+++ weapons/Laser.gd
@@ -1,5 +1,19 @@
 extends Area2D
 
+var beam = preload("res://weapons/LaserBeam.tscn")
+
+
+func _ready() -> void:
+	var newBeam1 = beam.instance()
+	var newBeam2 = beam.instance()
+	var beamPos1 = Vector2(position.x + 6, position.y)
+	var beamPos2 = Vector2(position.x + 12, position.y)
+	newBeam1.position = beamPos1
+	newBeam2.position = beamPos2
+	var weaponContainer = get_parent()
+	weaponContainer.add_child(newBeam1)
+	weaponContainer.add_child(newBeam2)
+
 
 func _on_Laser_body_entered(body: Node) -> void:
 	if body.name == "Player":
---
+++ weapons/LaserBeam.gd
@@ -0,0 +1,8 @@
+extends Area2D
+
+
+
+func _on_LaserBeam_body_entered(body: Node) -> void:
+	if body.is_in_group("Enemies"):
+		body.queue_free()
+		queue_free()
